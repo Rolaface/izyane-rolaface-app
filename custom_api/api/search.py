4625a5896f4a54d1a763af211fc1b198b9db317d
Abhishek Sati <abhishek.sati@rolaface.com>
2026-03-18 15:26:04 +0530

    updated API to get bank details

--- custom_api/api/search.py
+++ custom_api/api/search.py
@@ -2,37 +2,6 @@ from frappe.desk.search import build_for_autosuggest, search_widget
 import frappe
 from custom_api.utils.response import send_response
 
-
-@frappe.whitelist(allow_guest=False, methods=["GET"])
-def get_bank_accounts():
-    try:
-        txt = frappe.request.args.get("search", "")
-        results = search_widget(
-            "Bank",
-            txt.strip(),
-            None,
-            searchfield=None,
-            page_length=10,
-            filters=None,
-            filter_fields='["swift_number"]',
-            reference_doctype="Bank Account",
-            ignore_user_permissions=0,
-        )
-        response = build_for_autosuggest(results, doctype="Bank")
-        return send_response(
-            status="success",
-            message="Bank Accounts fetched successfully.",
-            data={"data": response},
-            status_code=200,
-            http_status=200,
-        )
-    except Exception as e:
-        frappe.log_error(frappe.get_traceback(), "Make Payment API Error")
-        return send_response(
-            status="fail", message=str(e), data=None, status_code=500, http_status=500
-        )
-
-
 @frappe.whitelist(allow_guest=False, methods=["GET"])
 def get_company_accounts():
     try:
@@ -238,15 +207,27 @@ def get_suppliers():
 def get_bank_company_supplier_cutomer():
     try:
         txt = frappe.request.args.get("search", "")
-        account_for = frappe.request.args.get("accountFor","")
-        if account_for == "Company":
+        doc_filter = frappe.request.args.get("accountFor","")
+
+        if doc_filter not in ["Company", "Supplier", "Bank", "Customer"]:
+            return send_response(
+                status="fail",
+                message="Invalid Filter.",
+                status_code=400,
+                http_status=400,
+            )
+
+        if doc_filter == "Company":
             response = frappe.defaults.get_user_default("Company")
         else:
             filter_fields = None
-            if account_for == "Supplier":
+            if doc_filter == "Supplier":
                 filter_fields = '["default_currency"]'
+            if doc_filter == "Bank":
+                filter_fields = '["swift_number"]'
+
             response = search_widget(
-                account_for,
+                doc_filter,
                 txt.strip(),
                 None,
                 searchfield=None,
