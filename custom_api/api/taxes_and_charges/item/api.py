9ff3c42d932e8880a5e42d9a376ec056c90489e7
Abhishek Sati <abhisheksati9876@gmail.com>
2026-04-08 15:41:12 +0530

    feat(tax-accounts): add endpoint to retrieve tax accounts with filtering options

--- custom_api/api/taxes_and_charges/item/api.py
+++ custom_api/api/taxes_and_charges/item/api.py
@@ -1,3 +1,4 @@
+from erpnext.controllers.queries import tax_account_query
 from custom_api.api.taxes_and_charges.item.service import get_item_tax_templates_service, upsert_item_tax_template
 import frappe
 from custom_api.utils.response import send_old_response, send_response_list
@@ -49,4 +50,35 @@ def get():
             message=str(e),
             status_code=500,
             http_status=500
+        )
+
+@frappe.whitelist()
+def get_tax_accounts(txt="", start=0, page_len=100):
+
+    company = frappe.defaults.get_user_default("Company")
+
+    accounts = tax_account_query(
+        doctype="Account",
+        txt=txt,
+        searchfield="name",
+        start=start,
+        page_len=page_len,
+        filters={
+            "company": company,
+            "account_type": ["Tax", "Chargeable", "Income Account",
+                              "Expenses Included In Valuation"
+                            ]
+        }
+    )
+
+    result = []
+    for acc in accounts:
+        result.append({"name": acc[0], "description": acc[1] })
+
+    return send_response_list(
+            status="success",
+            message="Tax accounts retrieved successfully",
+            data=result,
+            status_code=200,
+            http_status=200
         )
