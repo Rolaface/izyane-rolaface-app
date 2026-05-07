63265d7bcdc856482c5ca255007d7428a358a462
Abhishek Sati <abhisheksati9876@gmail.com>
2026-05-07 15:22:13 +0530

    protected all the routes of PI

--- custom_api/api/buying/purchase_invoice/api.py
+++ custom_api/api/buying/purchase_invoice/api.py
@@ -34,6 +34,7 @@ def get():
         )
 
 @frappe.whitelist(allow_guest = False, methods=["POST"])
+@require_permission("Purchase Invoice", "create")
 def create():
     data = frappe.local.form_dict
     try:
@@ -61,6 +62,7 @@ def create():
         )
 
 @frappe.whitelist(allow_guest=False, methods=["GET"])
+@require_permission("Purchase Invoice", "read")
 def get_by_id():
     try:
         pi_id = frappe.request.args.get("id")
@@ -101,6 +103,7 @@ def get_by_id():
         )
 
 @frappe.whitelist(allow_guest=False, methods=["PUT"])
+@require_permission("Purchase Invoice", "write")
 def update():
     try:
         data = frappe.local.form_dict
@@ -133,11 +136,11 @@ def update():
 
 @frappe.whitelist(allow_guest=False, methods=["PATCH"])
 def update_status():
+    temp_status = None
     try:
         data = frappe.request.get_json()
         pId = data.get("id")
         new_status = data.get("status")
-
         if not pId:
             return send_old_response(status="fail", message="'id' is required.", data=None, status_code=400, http_status=400)
 
@@ -164,13 +167,13 @@ def update_status():
         if new_status == "Submitted":
             if pi_doc.docstatus != 0:
                 return send_old_response(status="fail", message="Only Draft invoices can be submitted.", data=None, status_code=400, http_status=400)
+            temp_status = "Submit"
             pi_doc.submit()
-
         elif new_status == "Cancelled":
             if pi_doc.docstatus != 1:
                 return send_old_response(status="fail", message="Only Submitted invoices can be cancelled.", data=None, status_code=400, http_status=400)
+            temp_status = "Cancel"
             pi_doc.cancel()
-
         else:
             frappe.db.sql("""
                     UPDATE `tabPurchase Invoice`
@@ -192,6 +195,18 @@ def update_status():
             http_status=200
         )
 
+    except frappe.exceptions.PermissionError:
+        if db := getattr(frappe.local, "db", None):
+            db.rollback(chain=True)
+        else:
+            frappe.db.rollback()
+        return send_old_response(
+            status="fail",
+            message=f"You do not have permission to {temp_status} the status of this Purchase Invoice.Please contact your Administrator.",
+            data=None,
+            status_code=403,
+            http_status=403
+        )
     except Exception as e:
         frappe.db.rollback()
         frappe.log_error(frappe.get_traceback(), "Update Purchase Invoice Status Error")
