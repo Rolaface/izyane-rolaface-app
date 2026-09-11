3abf52406cb42edf6c33db6cbb195b78b0121789
Abhishek Sati <abhishek.sati@rolaface.com>
2026-09-11 11:01:48 +0200

    added PDC tags for PDC Mode of payment

--- custom_api/api/selling/sales_invoice/service.py
+++ custom_api/api/selling/sales_invoice/service.py
@@ -19,7 +19,7 @@ from .utils import (
     get_lpo_tax_template,
     get_zero_rated_tax_template
 )
-
+from frappe.desk.doctype.tag.tag import add_tag, remove_tag
 from custom_api.api.item.utils.item_utils import _get_tax
 
 def create_sales_invoice(data):
@@ -134,7 +134,8 @@ def create_sales_invoice(data):
     terms_payload = data.get("terms")
     if terms_payload:
         sync_invoice_terms(invoice, terms_payload)
-
+    if data.get("paymentMode") == "PDC":
+        add_tag("PDC", "Sales Invoice", invoice.name)
     return invoice
 
 def update_sales_invoice_customer(invoice, customer_id):
@@ -278,6 +279,11 @@ def update_sales_invoice(invoice_id, data):
         if detail:
             invoice.append("custom_details", detail)
 
+    if data.get("paymentMode") == "PDC":
+        add_tag("PDC", "Sales Invoice", invoice.name)
+    else:
+        remove_tag("PDC", "Sales Invoice", invoice.name)
+
     invoice.additional_discount_percentage = data.get("additional_discount_percentage")
     invoice.discount_amount = data.get("discount_amount")
     invoice.save(ignore_permissions=True)
@@ -349,7 +355,8 @@ def get_sales_invoice_by_id(invoice_id, is_credit_note=False, is_sales_debit_not
         "remarks": invoice.remarks,
         "additional_discount_percentage": invoice.additional_discount_percentage,
         "discount_amount": invoice.discount_amount,
-        "lpoNumber":invoice.po_no
+        "lpoNumber":invoice.po_no,
+        "tags": invoice._user_tags
     }
 
     payment_mode = custom_details[0].payment_mode if custom_details else None
@@ -567,6 +574,7 @@ def get_sales_invoices(filters=None, page=1, page_size=20, search=None):
             "cost_center",
             "status",
             "debit_to",
+            "_user_tags as tags"
         ],
         limit_start=start,
         limit_page_length=page_size,
